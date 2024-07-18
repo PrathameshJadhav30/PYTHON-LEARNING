604c13f2ff37c1964adf1ca9e2c5c43e30800fc2
Prathamesh <prathameshaj2004@gmail.com>
2024-07-18 20:40:30 +0530

    Changes

---
+++ PYTHON_100_DAYS_CHALLANGE/056-DAY-56-Intro-to-oops/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+RailwayForm   ---> Class [blueprint]
+# harry --> harry ki info wala form --> Object [entity]
+# tom --> tom ki info wala form --> Object [entity]
+# shubham -- shubham ki info wala form --> Object [entity]
+# # shubham.changeName("Shubhi")
